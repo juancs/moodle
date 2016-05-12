f77164e3c863213edd63673133dbfa37bf90a334
Juan Segarra Montesinos <juan.segarra@uji.es>
2016-05-12 09:44:11 +0200

    MDL-54106 user: Delete unconfirmed users not working because firstaccess == 0

--- lib/classes/task/delete_unconfirmed_users_task.php
+++ lib/classes/task/delete_unconfirmed_users_task.php
@@ -51,7 +51,7 @@ class delete_unconfirmed_users_task extends scheduled_task {
             $cuttime = $timenow - ($CFG->deleteunconfirmed * 3600);
             $rs = $DB->get_recordset_sql ("SELECT *
                                              FROM {user}
-                                            WHERE confirmed = 0 AND firstaccess > 0
+                                            WHERE confirmed = 0 AND firstaccess >= 0
                                                   AND firstaccess < ? AND deleted = 0", array($cuttime));
             foreach ($rs as $user) {
                 delete_user($user); // We MUST delete user properly first.
